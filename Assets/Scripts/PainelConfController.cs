a8bda1945f711c855a295bafdc33888feedbca52
Rosalie Alves <rosalieslv@gmail.com>
2025-02-10 18:57:30 -0300

    loadscene

--- Assets/Scripts/PainelConfController.cs
+++ Assets/Scripts/PainelConfController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PainelConfController : MonoBehaviour
 {
+    public string levelDeJogo;
+
     public GameObject player;
     PlayerLogic playerLogic;
-    
+
     [SerializeField] GameObject Conf;
 
     // Update is called once per frame
@@ -20,10 +23,15 @@ public class PainelConfController : MonoBehaviour
         }
     }
 
-    public void  CloseConf()
+    public void CloseConf()
     {
         playerLogic = player.GetComponent<PlayerLogic>();
         playerLogic.podesemover = true;
         Conf.SetActive(false);
+    }
+
+    public void RecarregarCena()
+    {
+        SceneManager.LoadScene(levelDeJogo);
     }
 }
