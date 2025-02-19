03c553393403f29419eb8446a7425260d80351b0
Rosalie Alves <rosalieslv@gmail.com>
2025-02-19 15:17:51 -0300

    .

--- Assets/Scripts/PainelConfController.cs
+++ Assets/Scripts/PainelConfController.cs
@@ -16,7 +16,7 @@ public class PainelConfController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Tab))
+        if (Input.GetKeyUp(KeyCode.Tab) || Input.GetKeyUp(KeyCode.Escape))
         {
             if (!isOpen)
             {
